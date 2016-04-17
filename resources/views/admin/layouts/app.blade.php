a3deb9f5bdf912608ece459d61eea6051e928121
Sarath <sarathiscookie@hotmail.com>
2016-04-17 22:27:23 +0530

    Vue JS has added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -46,12 +46,13 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-    @yield('script')
-
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="/js/holder.min.js"></script>
-
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- Vue JS -->
+    <script src="/js/vue.js"></script>
+
+    @yield('script')
+
   </body>
 </html>
