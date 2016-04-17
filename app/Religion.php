378b3daf2ecc62f8545df2a509c2af54dfd506e9
Sarath <sarathiscookie@hotmail.com>
2016-04-17 20:49:48 +0530

    Relation added in model page

--- app/Religion.php
+++ app/Religion.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Religion extends Model
 {
     protected $fillable = ['religion'];
+
+    public function caste()
+    {
+        return $this->hasMany('App\Caste');
+    }
+
+    public function subcaste()
+    {
+        return $this->hasMany('App\Subcaste');
+    }
 }
