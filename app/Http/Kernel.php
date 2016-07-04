be76dc3e23a12d2fba6dd6aff496d11a01fa9bff
Sarath <sarathiscookie@hotmail.com>
2016-07-04 20:44:33 +0530

    Kernel file updated

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,14 +31,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
         ],
 
-        'backend' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-        ],
-
         'api' => [
             'throttle:60,1',
         ],
